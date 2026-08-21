772e76a621aebadc55940e96fde573e565b64fd2
Nnadi <chimdijos8@gmail.com>
2026-08-21 16:39:00 +0100

    feat: remove unused legacy data migration module

--- videos/alive-launch/remotion.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-Config.setVideoImageFormat("jpeg");
-Config.setOverwriteOutput(true);
-Config.setConcurrency(2);
-Config.setTimeoutInMilliseconds(60000);
-Config.setBrowserExecutable("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
-Config.setChromiumDisableWebSecurity(true);
-Config.setChromiumOpenGlRenderer("swiftshader");
